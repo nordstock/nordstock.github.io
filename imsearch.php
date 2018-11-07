cb93874d4f314e1700d26f49a14a5aa81bd9efa0
Nordstock <nordstok@mail.ru>
2018-11-07 23:45:06 +0600

    1
    1

---
+++ imsearch.php
@@ -0,0 +1,162 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="ru-RU" dir="ltr">
+	<head>
+		<title>Поиск - NORDSTOCK</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="author" content="Lavr Monolight" />
+		<meta name="generator" content="Incomedia WebSite X5 Professional 14.0.3.1 - www.websitex5.com" />
+		<meta property="og:locale" content="ru" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://nordstock.github.io/imsearch.php" />
+		<meta property="og:title" content="Поиск" />
+		<meta property="og:site_name" content="NORDSTOCK" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" type="text/css" href="style/reset.css?14-0-3-1" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css?14-0-3-1" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css?14-0-3-1" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css?14-0-3-1" media="screen" />
+		<link rel="stylesheet" type="text/css" href="pcss/imsearch.css?14-0-3-1-636751536246817745" media="screen,print" />
+		<script type="text/javascript" src="res/jquery.js?14-0-3-1"></script>
+		<script type="text/javascript" src="res/x5engine.js?14-0-3-1" data-files-version="14-0-3-1"></script>
+		<script type="text/javascript">
+			window.onload = function(){ checkBrowserCompatibility('Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','Возможно, Ваш браузер не поддерживает функции, требуемые для визуализации этого Сайта.','[1]Обновите Ваш браузер[/1] или [2]продолжите процедуру [/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+			x5engine.boot.push(function () { x5engine.utils.imCodeProtection('Lavr Monolight'); });
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		<link rel="icon" href="favicon.png?14-0-3-1-636751536246787743" type="image/png" />
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<div id="imHeader">
+						<h1 class="imHidden">Поиск - NORDSTOCK</h1>
+						<div id="imHeaderObjects"><div id="imHeader_imMenuObject_02_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_2" class="" > <div id="imHeader_imCellStyleGraphics_2"></div><div id="imHeader_imCellStyle_2" ><div id="imHeader_imMenuObject_02"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Главная		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-5.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-5.html">
+Информация		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-3.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-3.html">
+Галерея		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/page-4.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-4.html">
+Контакты		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imHeader_imMenuObject_02',1000)});
+$(function () {$('#imHeader_imMenuObject_02 ul li').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imHeader_imMenuObject_02-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imHeader_imMenuObject_02-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div><div id="imHeader_imObjectImage_03_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_3" class="" > <div id="imHeader_imCellStyleGraphics_3"></div><div id="imHeader_imCellStyle_3" ><a href="index.html" onclick="return x5engine.utils.location('index.html', null, false)"><img id="imHeader_imObjectImage_03" src="images/232323.png" title="" alt="" /></a></div></div></div><div id="imHeader_imObjectImage_04_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_4" class="" > <div id="imHeader_imCellStyleGraphics_4"></div><div id="imHeader_imCellStyle_4" ><a href="index.html" onclick="return x5engine.utils.location('index.html', null, false)"><img id="imHeader_imObjectImage_04" src="images/233223.png" title="" alt="" /></a></div></div></div></div>
+					</div>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"><div id="imStickyBar_imMenuObject_01_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_1" class="" > <div id="imStickyBar_imCellStyleGraphics_1"></div><div id="imStickyBar_imCellStyle_1" ><div id="imStickyBar_imMenuObject_01"><div class="hamburger-button hamburger-component"><div><div><div class="hamburger-bar"></div><div class="hamburger-bar"></div><div class="hamburger-bar"></div></div></div></div><div class="hamburger-menu-background-container hamburger-component">
+	<div class="hamburger-menu-background menu-mobile menu-mobile-animated hidden">
+		<div class="hamburger-menu-close-button"><span>&times;</span></div>
+	</div>
+</div>
+<ul class="menu-mobile-animated hidden" data-flip-direction="1">
+	<li class="imMnMnFirst imPage" data-link-paths="/index.html,/">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="index.html">
+Главная		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-5.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-5.html">
+Информация		</a>
+</div>
+</div>
+	</li><li class="imMnMnMiddle imPage" data-link-paths="/page-3.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-3.html">
+Галерея		</a>
+</div>
+</div>
+	</li><li class="imMnMnLast imPage" data-link-paths="/page-4.html">
+<div class="label-wrapper">
+<div class="label-inner-wrapper">
+		<a class="label" href="page-4.html">
+Контакты		</a>
+</div>
+</div>
+	</li></ul></div><script type="text/javascript">
+x5engine.boot.push(function(){x5engine.initMenu('imStickyBar_imMenuObject_01',1000)});
+$(function () {$('#imStickyBar_imMenuObject_01 ul li').each(function () {    var $this = $(this), timeout = 0;    $this.on('mouseenter', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         clearTimeout(timeout);        setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeIn(); }, 250);    }).on('mouseleave', function () {        if($(this).parents('#imStickyBar_imMenuObject_01-menu-opened').length > 0) return;         timeout = setTimeout(function () { $this.children('ul, .multiple-column').stop(false, false).fadeOut(); }, 250);    });});});
+
+</script>
+</div></div></div><div id="imStickyBar_imObjectTitle_02_wrapper" class="template-object-wrapper"><div id="imStickyBar_imCell_2" class="" > <div id="imStickyBar_imCellStyleGraphics_2"></div><div id="imStickyBar_imCellStyle_2" ><div id="imStickyBar_imObjectTitle_02"><span id ="imStickyBar_imObjectTitle_02_text" >Stairway</span > </div></div></div></div></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Заголовок главного меню">Перейти к контенту</a>
+					<div id="imSideBarContainer">
+						<div id="imSideBarGraphics"></div>
+						<div id="imSideBar">
+							<div id="imSideBarObjects"></div>
+						</div>
+					</div>
+					<div id="imContentContainer">
+						<div id="imContentGraphics"></div>
+						<div id="imContent">
+							<a id="imGoToCont"></a>
+				<div id="imSearchPage">
+				<h2 id="imPgTitle">Результаты поиска</h2>
+				<?php
+				$search = new imSearch();
+				$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
+				$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+				$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+				<div class="searchPageContainer">
+				<?php echo $search->search($keys, $page, $type); ?>
+				</div>
+				</div>
+				
+							<div class="imClear"></div>
+						</div>
+					</div>
+					<div id="imFooter">
+						<div id="imFooterObjects"></div>
+					</div>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Прочесть эту страницу заново">Назад к содержимому</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Для использования этого сайта необходимо включить JavaScript.</div></noscript>
+	</body>
+</html>
---
+++ res/access.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 'tuj175le';
+$imSettings['access']['users'] = array(
+	'34324234@mail.ru' => array(
+		'groups' => array('ukgf9jo6'),
+		'id' => '7g57icq6',
+		'firstname' => 'Admin',
+		'lastname' => '',
+		'password' => 'aqs2178v',
+		'email' => '34324234@mail.ru',
+		'page' => false
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('7g57icq6');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
